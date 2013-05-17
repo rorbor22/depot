3f252fed431de17398320ad3ac671bf8e5bf1d0c
ror <shohin_b@www.mail.ru>
2013-05-17 11:24:26 +0400

    Begin-depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4191c312e666628ab5c42ade09dd038dcb98f06b577e27d4cb822ddaa8d2071a5e90d4d81a83fc90f7de5e2c32ec988092f0c6273abf39111de0ceb0d9b58a63'
